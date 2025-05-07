e8af236c395404da45b8f3345c09f8b11cf7ebaa
Kevin Masters <irimiuke@gmail.com>
2025-05-07 13:56:19 -0400

    finish adding scaffolded components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MoodBoardEditor from "./pages/MoodBoardEditor";
+import MoodBoardView from "./pages/MoodBoardView";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/board/new" element={<MoodBoardEditor />} />
+          <Route path="/board/:id/edit" element={<MoodBoardEditor />} />
+          <Route path="/board/:id/view" element={<MoodBoardView />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
+
 
 export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Navbar = () => (
+  <nav className="flex gap-4 bg-gray-100 p-4">
+    <Link to="/">Home</Link>
+    <Link to="/board/new">Create New Board</Link>
+  </nav>
+);
 
-export default Navbar
+export default Navbar;

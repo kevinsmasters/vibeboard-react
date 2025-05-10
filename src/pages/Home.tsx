27325a3f6db8a181a035ae8b3b6990fb9a3d25fb
Kevin Masters <irimiuke@gmail.com>
2025-05-10 09:48:26 -0400

    add more tiles to get ready for dragging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,11 @@ const sampleTiles: MoodTileType[] = [
   { id: "2", type: "quote", content: "Live by the sun, love by the moon." },
   { id: "3", type: "color", content: "#f0a" },
   { id: "4", type: "music", content: "https://open.spotify.com/embed/track/4uLU6hMCjMI75M1A2tKUQC" },
+  { id: "5", type: "music", content: "https://open.spotify.com/embed/track/5hJFSRXMut9SnlNl8Sj4S8" },
+  { id: "6", type: "color", content: "#fc0" },
+  { id: "7", type: "image", content: "https://placecage.lucidinternets.com/400/300" },
+  { id: "8", type: "image", content: "https://placecage.lucidinternets.com/g/200/300" },
+  { id: "9", type: "music", content: "https://open.spotify.com/embed/track/2p7HareTeAv9kzrBpzGQVG" },
 ];
 
 const Home: React.FC = () => {
